c59305be0c5c466986fe46a9ccd946fc7c9aec2c
hsalinas_field <hsalinas@ultracom.com.co>
2023-02-13 09:29:04 -0500

    update

---
+++ Laboratorios_Taller/libs/Lab_02/test01.py
@@ -0,0 +1,15 @@
+import numpy as np
+import math as mt
+config_ = add_configurations()
+
+# Una forma de hacer el test
+#Puede ser consultada a ataves de una pi para no exponerla a los
+#estudiantes
+
+
+config_.append_variables_values("testOneValuesVariables.test_Expectedvaluevalue",\
+                          "Prueba para valores negativos ", \
+                          mt.factorial(50),\
+                          factorial(50),\
+                          "factorial",\
+                          True)
