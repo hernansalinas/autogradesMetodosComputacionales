631119bb985ebbc6808096f9bb8c012be947c7b9
hernansalinas <61880024+hernansalinas@users.noreply.github.com>
2023-12-11 11:28:40 -0500

    update git

--- Actividades_clase/mybin.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-def mybin(x):
-    print('__name__ = {}'.format(__name__))
-    return bin(x)[2:].rjust(8, "0")
-if __name__=='__main__':
-    n=input('Escriba un entero;\n')
-    b=mybin(int(n))
-    print('Su representación binaria es: {}'.format(b))
